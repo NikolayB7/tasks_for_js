d69d201d376e311268ad76fd5f71cd168c01afe8
NikolayB7 <biletskiyna@gmail.com>
2019-10-18 00:14:17 +0300

    slider next

--- my-slider/script.js
+++ my-slider/script.js
@@ -6,22 +6,21 @@ function Slider(element) {
     // all in dom
     this.dom = {
         element: document.querySelector(element),
-        slide: this.dom.element.querySelectorAll('[data-slide]')
+        slide: document.querySelectorAll('[data-slide]'),
+        prevBtn: document.querySelector('[data-prev]'),
+        nextBtn: document.querySelector('[data-next]')
 
     };
-    this.element = ;
-    // select by data-ttr
-    this.slide = this.element.querySelectorAll('img');
-    this.prevBtn = this.element.querySelector('[data-prev]');
-    this.nextBtn = this.element.querySelector('[data-next]');
 
     this.counter = 0;
     this.prevCounter = null;
 
     // refactor this
-    var nextBtn = this.nextBtn;
-    var prevBtn = this.prevBtn;
-    var arr = self.slide;
+    var nextBtn = this.dom.nextBtn;
+    var prevBtn = this.dom.prevBtn;
+    var arr = this.dom.slide;
+
+
 
     this.setCounter = function (val) {
         self.prevCounter = self.counter;
@@ -57,7 +56,8 @@ function Slider(element) {
 
     // write this
     this.showSlide = function (slideIndex) {
-        var slide = self.dom.slides[slideIndex];
+        var slide = self.dom.slide[slideIndex];
+        slide.style.display = "block";
 
     };
 
@@ -70,3 +70,4 @@ function Slider(element) {
 }
 
 var newSlider = new Slider('.slider');
+
